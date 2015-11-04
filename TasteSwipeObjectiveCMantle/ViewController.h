440b49156967d77388ee4b8bb176c5720c7ea442
DanielSon105 <daniel.g.barrido@gmail.com>
2015-11-04 12:55:04 -0600

    Initial Commit

---
+++ TasteSwipeObjectiveCMantle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TasteSwipeObjectiveCMantle
+//
+//  Created by Daniel Barrido on 11/4/15.
+//  Copyright © 2015 Daniel Barrido. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TasteSwipeObjectiveCMantle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TasteSwipeObjectiveCMantle
+//
+//  Created by Daniel Barrido on 11/4/15.
+//  Copyright © 2015 Daniel Barrido. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
